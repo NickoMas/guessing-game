0e621539ff7514b3741fbe75197cb1a269d092f8
NickoMas <aquinnm@gmail.com>
2017-02-02 22:04:53 +0200

    Implemented methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+    }
 
     setRange(min, max) {
-
+      for(var i=min;i<=max;i++){    
+      this.arr[i] = i;    
+      }
+      return this;
     }
 
     guess() {
-
+      var l = this.arr.length;
+      var mid = Math.floor(l/2);
+      return this.arr[mid];
     }
 
     lower() {
-
+      var l = this.arr.length;
+      var mid = Math.floor(l/2);
+      this.arr = this.arr.slice(0,mid+1);
+      return this;
     }
 
     greater() {
-
+      var l = this.arr.length;
+      var mid = Math.floor(l/2);
+      this.arr = this.arr.splice(mid);
+      return this;
     }
 }
 
